fc9c8dc1c032854f504360bc778d3a754dff7010
Matei Nicolae - Daniel <matei1337@icloud.com>
2022-09-21 11:12:31 +0300

    generated necessary components for firebase auth.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Firebase.
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { VerifyEmailAddressComponent } from './verify-email-address/verify-email-address.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, DashboardComponent, ForgotPasswordComponent, SignInComponent, SignUpComponent, VerifyEmailAddressComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		provideFirebaseApp(() => initializeApp(environment.firebase)),
+		provideFirestore(() => getFirestore()),
+  BrowserAnimationsModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+	firebase: {
+		projectId: 'ng-blog-42689',
+		appId: '1:894301750428:web:5450bcf3bcd470ebfcd959',
+		storageBucket: 'ng-blog-42689.appspot.com',
+		apiKey: 'AIzaSyBeC_7CGyGJeNpnRMjrvxvVuhfhm4NIUik',
+		authDomain: 'ng-blog-42689.firebaseapp.com',
+		messagingSenderId: '894301750428',
+		measurementId: 'G-WP8D3BMPE4',
+	},
+	production: true,
 };
