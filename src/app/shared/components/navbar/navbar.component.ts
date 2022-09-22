a71faefc1163bd7bc34ca6292cda2953a702cab2
Matei Nicolae - Daniel <matei1337@icloud.com>
2022-09-22 16:56:24 +0300

    added onPageScroll() to check when an user is scrolling, to change the background-color of the navbar and add a shadow to it.

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 // Import Authentification service.
 import { AuthService } from '../../../shared/services/auth/auth.service';
@@ -14,4 +14,17 @@ export class NavbarComponent implements OnInit {
 	constructor(public authService: AuthService) {}
 
 	ngOnInit(): void {}
+
+	@HostListener('window:scroll', ['$event'])
+	onPageScroll = () => {
+		let element = document.querySelector('.navbar') as HTMLElement;
+
+		if (window.pageYOffset > element.clientHeight) {
+			element.classList.add('navbar-inverse');
+			element.classList.add('mat-elevation-z8');
+		} else {
+			element.classList.remove('navbar-inverse');
+			element.classList.remove('mat-elevation-z8');
+		}
+	};
 }
