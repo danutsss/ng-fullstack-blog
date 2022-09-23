54ab8d51e23ccc416028e97b33b0e314bbbf5fc6
Matei Nicolae - Daniel <matei1337@icloud.com>
2022-09-23 14:54:46 +0300

    footer design created

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {
+	faLinkedin,
+	faGithub,
+	faFacebook,
+	faTwitter,
+	faGoogle,
+	faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+	selector: 'app-footer',
+	templateUrl: './footer.component.html',
+	styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+	public faLinkedin: any = faLinkedin;
+	public faGithub: any = faGithub;
+	public faFacebook: any = faFacebook;
+	public faTwitter: any = faTwitter;
+	public faGoogle: any = faGoogle;
+	public faInstagram: any = faInstagram;
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
