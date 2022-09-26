7d2b4562081cfdf7e36e0cfbb14b4dfbb85aa441
Matei Nicolae - Daniel <matei1337@icloud.com>
2022-09-26 18:08:56 +0300

    post service complete.

--- src/app/shared/services/post/post.service.ts
+++ src/app/shared/services/post/post.service.ts
@@ -10,12 +10,15 @@ import {
 // Import Post model.
 import { Post } from '../models/post';
 
+// Import rxJs map operator.
+import { map } from 'rxjs/operators';
+
 @Injectable({
 	providedIn: 'root',
 })
 export class PostService {
 	postsCollection: AngularFirestoreCollection<Post>;
-	postDoc: AngularFirestoreDocument<Post>;
+	postDoc!: AngularFirestoreDocument<Post>;
 	constructor(private afs: AngularFirestore) {
 		this.postsCollection = this.afs.collection('posts', (ref) =>
 			ref.orderBy('date', 'desc')
@@ -26,21 +29,56 @@ export class PostService {
 	 * Get all posts.
 	 */
 	getPosts() {
-		return this.postsCollection.snapshotChanges().map((actions: any) => {
-			return actions.map((action: any) => {
-				const data = action.payload.doc.data() as Post;
-				const id = action.payload.doc.id;
-				return { id, ...data };
-			});
-		});
+		return this.postsCollection.snapshotChanges().pipe(
+			map((actions: any) => {
+				return actions.map((action: any) => {
+					const data = action.payload.doc.data() as Post;
+					const id = action.payload.doc.id;
+					return { id, ...data };
+				});
+			})
+		);
 	}
 
 	/**
 	 * Get post data by id.
 	 * @param id - Post ID.
 	 */
-	getPost(id: string) {
+	getPosteData(id: string) {
 		this.postDoc = this.afs.doc<Post>(`posts/${id}`);
 		return this.postDoc.valueChanges();
+	}
+
+	/**
+	 * Get post by id.
+	 * @param id - Post ID.
+	 */
+	getPost(id: string) {
+		return this.afs.doc<Post>(`posts/${id}`);
+	}
+
+	/**
+	 * Create new post.
+	 * @param data - Post data.
+	 */
+	createPost(data: Post) {
+		return this.postsCollection.add(data);
+	}
+
+	/**
+	 * Delete post.
+	 * @param id - Post ID.
+	 */
+	deletePost(id: string) {
+		return this.getPost(id).delete();
+	}
+
+	/**
+	 * Update post.
+	 * @param id - Post ID.
+	 * @param data - Post data.
+	 */
+	updatePost(id: string, data: Post) {
+		return this.getPost(id).update(data);
 	}
 }
