9130179a13c41d32c21c338ed08fe04456dac9a3
danutsss <matei1337@icloud.com>
2022-09-30 15:49:15 +0300

    modified the width of the create post dialog

--- src/app/shared/components/create-post-dialog/create-post-dialog.component.ts
+++ src/app/shared/components/create-post-dialog/create-post-dialog.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+// Import Angular Firestore.
+import {
+	AngularFirestore,
+	AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
+
+// Import post & auth service.
+import { PostService } from '../../../shared/services/post/post.service';
+import { AuthService } from '../../../shared/services/auth/auth.service';
 
 @Component({
 	selector: 'app-create-post-dialog',
@@ -6,7 +17,40 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./create-post-dialog.component.scss'],
 })
 export class CreatePostDialogComponent implements OnInit {
-	constructor() {}
+	content!: string;
+	image!: string;
+	title!: string;
+
+	saving = 'Creeaza postare';
+
+	constructor(
+		private postService: PostService,
+		public auth: AuthService,
+		public afStore: AngularFirestore
+	) {}
 
 	ngOnInit(): void {}
+
+	createPost() {
+		let newSlug = this.title
+			.toLowerCase()
+			.replace(/ /g, '-')
+			.replace(/[^\w-]+/g, '');
+
+		const postData = {
+			content: this.content,
+			title: this.title,
+			author: this.auth.userData.displayName || this.auth.userData.email,
+			authorId: this.auth.userData.uid,
+			published: new Date(),
+			slug: newSlug,
+		};
+		this.postService.createPost(postData);
+		this.title = '';
+		this.content = '';
+		this.image = '';
+
+		this.saving = 'Postarea a fost salvata.';
+		setTimeout(() => (this.saving = 'Creeaza postare'), 3000);
+	}
 }
