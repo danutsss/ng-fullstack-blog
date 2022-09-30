cbf8b3cba285303960dfbf1b19a284be345cc219
danutsss <matei1337@icloud.com>
2022-09-30 15:47:58 +0300

    created post detail component which contains all information about the post (title, content etc.)

---
+++ src/app/components/home/post-detail/post-detail.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+// Import post service.
+import { PostService } from 'src/app/shared/services/post/post.service';
+import { Post } from 'src/app/shared/services/models/post';
+
+@Component({
+	selector: 'app-post-detail',
+	templateUrl: './post-detail.component.html',
+	styleUrls: ['./post-detail.component.scss'],
+})
+export class PostDetailComponent implements OnInit {
+	post!: Post;
+	constructor(private postService: PostService, private route: ActivatedRoute) {}
+
+	ngOnInit(): void {
+		this.getPost();
+	}
+
+	getPost() {
+		const id = this.route.snapshot.paramMap.get('id') as string;
+		this.postService
+			.getPostData(id)
+			.subscribe((post) => (this.post = post as Post));
+	}
+}
