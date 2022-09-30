9506f9882b1fb1d890e7e92fbee212eb8a55efcb
danutsss <matei1337@icloud.com>
2022-09-30 15:51:35 +0300

    removed ackgroundImage from Post interface.

--- src/app/shared/services/models/post.ts
+++ src/app/shared/services/models/post.ts
@@ -1,10 +1,9 @@
 export interface Post {
 	id?: string;
-	slug: string;
-	backgroundImage: string;
 	title: string;
 	content: string;
 	author: string;
 	authorId: string;
-	published: Date;
+	published: any;
+	slug: string;
 }
