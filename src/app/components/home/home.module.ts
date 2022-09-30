d0d5a4a72ca91cb26d6b9188d85306e71aed1f5a
danutsss <matei1337@icloud.com>
2022-09-30 15:50:06 +0300

    home component transformed into a module

---
+++ src/app/components/home/home.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+// Import Angular Material components.
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
+// Import components.
+import { PostListComponent } from './post-list/post-list.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+
+// Import post service.
+import { PostService } from 'src/app/shared/services/post/post.service';
+
+const routes: Routes = [
+	{
+		path: '',
+		pathMatch: 'full',
+		component: PostListComponent,
+	},
+	{
+		path: ':id',
+		component: PostDetailComponent,
+	},
+	{
+		path: 'page/:pageNum',
+		component: PostListComponent,
+	},
+];
+
+@NgModule({
+	declarations: [PostListComponent, PostDetailComponent],
+	imports: [
+		CommonModule,
+		MatButtonModule,
+		MatCardModule,
+		MatChipsModule,
+		FlexLayoutModule,
+		MatIconModule,
+		MatDialogModule,
+		MatFormFieldModule,
+		MatSelectModule,
+		FormsModule,
+		RouterModule.forChild(routes),
+	],
+	providers: [PostService],
+})
+export class HomeModule {}
