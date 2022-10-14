ea1bf1c5c583f33662b1ab9465cc8dc4632d4552
Matei Nicolae - Daniel <matei1337@icloud.com>
2022-10-14 15:47:33 +0300

    removed unused angular firestore import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-// Inject Angular Firestore.
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-
 // Inject Auth service.
 import { AuthService } from './shared/services/auth/auth.service';
 
