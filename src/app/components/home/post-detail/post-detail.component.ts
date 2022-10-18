2e471f08c6d57866f677647a5e47f6fad55f053e
Matei Nicolae - Daniel <matei1337@icloud.com>
2022-10-18 23:04:58 +0300

    removed unused import

--- src/app/components/home/post-detail/post-detail.component.ts
+++ src/app/components/home/post-detail/post-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, SecurityContext } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 // Import post service.
