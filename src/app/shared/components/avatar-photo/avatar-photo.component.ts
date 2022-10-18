60b9c638c94eb7c6830ab2989ee575becbc1c732
Matei Nicolae - Daniel <matei1337@icloud.com>
2022-10-19 00:47:41 +0300

    removed unused comment service import

--- src/app/shared/components/avatar-photo/avatar-photo.component.ts
+++ src/app/shared/components/avatar-photo/avatar-photo.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-import { CommentsService } from '../../services/comments/comments.service';
-
 @Component({
 	selector: 'app-avatar-photo',
 	templateUrl: './avatar-photo.component.html',
@@ -26,7 +24,7 @@ export class AvatarPhotoComponent implements OnInit {
 		'#CDDC39',
 		'#FFEB3B',
 	];
-	constructor(private commentService: CommentsService) {}
+	constructor() {}
 
 	ngOnInit(): void {
 		this.createInitials();
