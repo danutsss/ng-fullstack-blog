fe310d38cbfab02284e15f938bbb6e6a90a852a3
Matei Nicolae - Daniel <matei1337@icloud.com>
2022-12-15 16:05:48 +0200

    removed the icons imports from footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,12 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {
-	faLinkedin,
-	faGithub,
-	faFacebook,
-	faTwitter,
-	faGoogle,
-	faInstagram,
-} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
 	selector: 'app-footer',
@@ -14,12 +6,6 @@ import {
 	styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-	public faLinkedin: any = faLinkedin;
-	public faGithub: any = faGithub;
-	public faFacebook: any = faFacebook;
-	public faTwitter: any = faTwitter;
-	public faGoogle: any = faGoogle;
-	public faInstagram: any = faInstagram;
 	constructor() {}
 
 	ngOnInit(): void {}
